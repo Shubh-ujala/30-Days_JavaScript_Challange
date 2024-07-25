d1ee756bb66c95f99fb1deb5839d299546d4073a
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2024-07-25 15:04:19 +0530

    Delete Task_02.js

--- Task_02.js
+++
@@ -1,8 +0,0 @@
-//Task 2: Create a module that exports an object representing a person with properties and methods. Import and use this module in another script.
-
-export const userDetails = {
-    name:"XYZ",
-    email:"XYZ@google.com",
-    contact: 12384799,
-    password: "xyz@3287950"
-}
