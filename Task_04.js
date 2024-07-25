1ddd708639d93700772e44aeef524dcf2aa2f7af
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2024-07-25 15:05:04 +0530

    Delete Task_04.js

--- Task_04.js
+++
@@ -1,6 +0,0 @@
-//Task 4: Create a module that exports a single function using default export. Import and use this function in another script.
-
-export default function sayHello (name){
-    console.log(`Hey! there ${name}`);
-}
-
